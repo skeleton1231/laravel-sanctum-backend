c6bd5b0a51c47332f63d8d97c768041c932157d3
skeleton1231 <814010079@qq.com>
2023-06-24 23:15:53 +0800

    feat:billing|payment

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class BillingController extends Controller
 {
-    //
+    // show the billing list with laravel sanctum  and json response
+    public function index(Request $request)
+    {
+        return response()->json([
+            'billing' =>$request->user()->billing()
+        ]);
+    }
 }
---
+++ database/migrations/2023_06_24_150458_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('stripe_id')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four', 4)->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
